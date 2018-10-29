0444a2115a8187173314818dece030b462ca88a5
alex197 <alexolajide197@gmail.com>
2018-10-29 12:46:17 +0100

    Student data base

---
+++ studentdb/api/controllers/StudentdbController.js
@@ -0,0 +1,88 @@
+/**
+ * StudentdbController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  // to view student record
+  records:function(req, res){
+       Studentdb.find({}).exec(function(err, studentdb){
+          if(err) {
+               res.send(500,{error:'Database error'});
+        
+              
+           }
+   return res.view('records', {studentdb:studentdb});
+     });
+   },
+  add:function(req, res){
+  return res.view('add') 
+  },
+  create:function(req, res){
+     var title = req.body.title;
+     var firstname = req.body.firstname;
+     var lastname = req.body.lastname;
+     var sex = req.body.sex;
+     var age = req.body.age;
+     var course = req.body.course;
+     var level = req.body.level;
+     var cgpa = req.body.cgpa;
+     var body =  req.body.body;
+
+    Studentdb.create({title:title, firstname:firstname, lastname:lastname, sex:sex,
+       age:age, course:course, level:level, cgpa:cgpa,  body:body}).exec(function(err){
+      if(err) {
+        res.send(500,{error:'Database error'});
+      }  
+      res.redirect('/studentdb/records');
+    });
+  },
+
+  delete:function(req, res){
+      
+    Studentdb.destroy({id:req.params.id}).exec(function(err){
+      if(err) {
+        res.send(500,{error:'Database error'});
+      }  
+       res.redirect('/studentdb/records')
+    });
+    return false;
+  },
+  
+ edit:function(req, res){
+
+    Studentdb.findOne({id:req.params.id}).exec(function(err,studentdb){
+      if(err) {
+        res.send(500,{error:'Database error'});
+      }  
+      return res.view('edit' , {studentdb:studentdb});
+    });
+   },
+  update:function(req, res){
+
+    var title = req.body.title;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var sex = req.body.sex;
+    var age = req.body.age;
+    var course = req.body.course;
+    var level = req.body.level;
+    var cgpa = req.body.cgpa;
+    var body =  req.body.body;
+
+    Studentdb.update({id: req.params.id},{title:title, firstname:firstname, lastname:lastname, sex:sex,
+      age:age, course:course, level:level, cgpa:cgpa,  body:body}).exec(function(err){
+      if(err) {
+        res.send(500,{error:'Database error'});
+      }  
+      res.redirect('/studentdb/records');
+    });
+     return false;
+  }
+};
+
+
+
+
---
+++ studentdb/api/models/Studentdb.js
@@ -0,0 +1,42 @@
+/**
+ * Studentdb.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        ::  https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+title:{
+  type:'string'
+},
+firstname:{
+  type:'string'
+},
+lastname:{
+  type:'string'
+},
+sex:{
+  type:'string'
+},
+age:{
+  type:'string'
+},
+course:{
+  type:'string'
+},
+level:{
+  type:'string'
+},
+cgpa:{
+  type:'string'
+},
+body: {
+  type: 'string'
+}
+
+  },
+};
+
